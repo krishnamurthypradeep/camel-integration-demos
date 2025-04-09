49d47b496a021877d2713608aafdcfda585ca0b7
pradeepkrishnamurthy <pradeeepmurthy@gmail.com>
2025-04-09 12:48:53 +0530

    MultiCast2

--- src/main/java/com/myapp/camel/routes/MulticastPattern.java
+++ src/main/java/com/myapp/camel/routes/MulticastPattern.java
@@ -10,10 +10,18 @@ public class MulticastPattern  extends RouteBuilder{
 	public void configure() throws Exception {
 		// TODO Auto-generated method stub
 	
+		// apache camel is going to threadpool 
+		
 		from("activemq:queue:activeOrders")
 		
 		.multicast()
+		.parallelProcessing()
+		.stopOnException()
 		.to("activemq:queue:accounts","activemq:queue:production");
+		
+//		from("activemq:queue:activeOrders")
+//		.unmarshall().pgp(new File("privatekey"),"")
+//		.log("")
 		
 	}
 
